92482c5e49bc56a2b29d6ac94d69555961da5a67
Gergely Takács <gergelytakacs@gergelytakacs.com>
2019-07-11 10:18:51 +0200

    Update simpleTest.m

--- simpleTest.m
+++ simpleTest.m
@@ -1,6 +1,13 @@
-A = rand(5)
-B = rand(5)
-I = eye(5)
-C = A./(B+I)
-c=sqrt(10);
-thisIsABug
+% Simple MATLAB / Octave Code to test Travis CI builds using
+% the .travis.yml file in the root directory of this repository.
+% Gergely Takács, 2019, www.gergelytakacs.com
+
+%% Correct operations to test
+	A = rand(5)
+	B = rand(5)
+	I = eye(5)
+	C = A./(B+I)
+	c=sqrt(10);
+
+%% Uncomment to create a bug artificially:
+% 	thisIsABug
